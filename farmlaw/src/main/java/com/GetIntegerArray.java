4d8b7bffbbb936de49f5c7c1617199b0fc098748
Poonam Gupta <spoonamprasad400@gmail.com>
2021-05-13 19:39:26 +0530

    Signed-off-by: Poonam Gupta <spoonamprasad400@gmail.com>
    Message:PG:Message from Poonam

--- farmlaw/src/main/java/com/GetIntegerArray.java
+++ farmlaw/src/main/java/com/GetIntegerArray.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GetIntegerArray {
 @GetMapping("/arrays")
 public int[]ArrayAPI3(){
-int array[]= {10,20,30};
+int array[]= {10,20,40};
 return array;
 }
 
